8f34c0bb38c105f7a97b32cf112336759fae3f5f
RustyBamboo <dvolya@gmail.com>
2017-11-10 12:59:49 -0500

    Modifications to enable tshirt-cannon mechanisms

--- Robot_Victor.java
+++ Robot_Victor.java
@@ -3,58 +3,53 @@ package org.usfirst.frc.team6184.robot;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
+//import edu.wpi.first.wpilibj.RobotDrive;
+//import edu.wpi.first.wpilibj.AnalogInput;
+//import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
 
-//Simple drive program for robot
-//Allows rotation and basic backward forward movement
+//import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends SampleRobot {
 
-    Joystick stick = new Joystick(0); // set to ID 1 in DriverStation
+    Joystick stick = new Joystick(0); // set to ID 0 in DriverStation
 
-    Victor mR = new Victor(0), mL = new Victor(1), shoot = new Victor(3), actu = new Victor(2), climb = new Victor(4);
-                                                                                            // the
-                                                                                            // motor
-                                                                                        // controllers
+    // Declare our motor controllers using their PWM ports from RoboRIO
+    Victor mR = new Victor(0), mL = new Victor(1), tilt_shooter = new Victor(2), rotate_shooter = new Victor(3);
 
-    // Solenoid tshirtSolenoid = new Solenoid(0); //Assumes 0 is the ID of the
-    // Pnemuatic Controller and that we are using the 1st output
+    // Pass 0 as the port number of the Pnemuatic Controller in the Control Area Network (CAN) 
+    // and also we are inherently assuming a default Node ID of 0
+    Solenoid tshirtSolenoid = new Solenoid(0);
 
-    // AnalogInput ultraSonic = new AnalogInput(0); //Assumes 0 for the Analog
-    // Input on RoboRIO
-    // final double ultraInputVoltage = 5.0f; //What's the voltage input for
-    // HRLV-MaxSonar-EZ1 relative to Pin 7 (GND)
-    // final double ultraScaling = ultraInputVoltage/5120; //(about 0.977mV per
-    // 1mm at 5V input voltage) Scaling specs for HRLV-MaxSonar-EZ1
-
-    // DigitalOutput ultraOut = new DigitalOutput(0);
-
+    // Initialize things in the class constructor
     public Robot() {
-
     }
 
+    // This will run when the robot is in TeleOp mode
     @Override
     public void operatorControl() {
-        climb.setSpeed(0);
         while (isOperatorControl() && isEnabled()) {
-             //ultraOut.set(true);
-             //System.out.println(getDistanceUltra()); //Print distance in mm
-           
+            
+            // Rotate the tshirt cannon using buttons 3 and 4
             boolean isclickedright = stick.getRawButton(3);
             boolean isclickedleft = stick.getRawButton(4);
-            if (isclickedright && ! isclickedleft){ //turn right
-                climb.setSpeed(.3);
+            if (isclickedright){
+                rotate_shooter.setSpeed(1);
             }
-            else if (!isclickedright && isclickedleft){ //turn left
-                climb.setSpeed(-.3);
+            else if (!isclickedright && isclickedleft){
+                rotate_shooter.setSpeed(-1);
             }
-                else climb.setSpeed(0);
-            //I Was Here
-           
+               else rotate_shooter.setSpeed(0);
+
+           // Use the little lever on joystick to control tilt
+           tilt_shooter.setSpeed(stick.getX());
+
+           // Open the solenoid if button 5 is clicked
+           if(stick.getRawButton(5)) tshirtSolenoid.set(true);
+           else tshirtSolenoid.set(false);
+
+
+           // Drive the robot using the 2 drive-train motors
             double m;
             if (stick.getRawButton(3))
                 m = 1; // Boost power
@@ -72,78 +67,12 @@ public class Robot extends SampleRobot {
                 mR.setSpeed(0);
                 mL.setSpeed(0);
             }
-            //if(stick.getRawButton(1)){
-                //shoot.setSpeed(1);
-                //actu.setSpeed(.22);
-            } //else {
-                //shoot.setSpeed(0);
-                //actu.setSpeed(0);
-            }
-            //if(stick.getRawButton(8)){
-                //climb.setSpeed(.5);
-            //} //else {
-                //climb.setSpeed(0);
-            //}
-        //}
-       
-    public void stop() {
-        mR.setSpeed(0);
-        mL.setSpeed(0);
+         }
     }
-    public void stopShooter() {
-        shoot.setSpeed(0);
-        actu.setSpeed(0);
-    }
+
+
+    // This will run when robot is in autonomous mode
     @Override
     public void autonomous() {
-        double m = .7;
-        mR.setSpeed(0.5 * m);
-        mL.setSpeed(-0.5 * m);
-        try {
-            wait(1);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        stop();
-       
-        //Rotate right
-        mR.setSpeed(0.2*m);
-        mL.setSpeed(0.2*m);
-       
-        try {
-            wait(1);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-       
-        stop();
-       
-        //Shoot
-       
-        shoot.setSpeed(1);
-        actu.setSpeed(.1);
-       
-        try {
-            wait(13);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-       
-        stopShooter();
     }
-
-    // private void solenoidController() {
-    // if(stick.getRawButton(1)) tshirtSolenoid.set(true);
-    // else tshirtSolenoid.set(false);
-    // }
-
-    // Uses Ultrasonic sensor to find distance in mm
-    // private double getDistanceUltra() {
-    // double volts = ultraSonic.getVoltage();
-    // ultraOut.set(false);
-    // return volts/ultraScaling;
-    // }
 }
